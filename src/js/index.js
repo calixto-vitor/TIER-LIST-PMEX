a7abb233d3e7ffac0f45d5301f6f2d30c5a18053
Vitor Calixto <calixtovitor33@gmail.com>
2022-10-01 10:49:45 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
